eb9422a62bd84fad105538ad4050058c98713b21
Paul Sosulin <pavelsosulin@gmail.com>
2022-06-25 13:47:50 +0400

    Tetris game day 3(fixed code errors)

--- online/tetris.py
+++ online/tetris.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 # создание структуры данных для игровых фигур
 # установка глобальных переменных
 # функции
@@ -11,7 +12,7 @@ import random
 # - код главной функции main
 
 """
-прямоугольная сетка для фигур игры размером 10 x 20, таких как:
+квадратная сетка для фигур игры размером 10 x 20, таких как:
 S, Z, I, O, J, L, T,
 представленных в порядке от 0 до 6 
 """
@@ -128,7 +129,14 @@ T = [['.....',
       '..0..',
       '.00..',
       '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '..0..',
+      '..00.',
+      '..0..',
       '.....']]
+
 # список основных фигур игры
 shapes = [S, Z, I, O, J, L, T]
 # список цветов основных фигур
@@ -180,13 +188,13 @@ def convert_shape_format(shape):
 # функция проверки правильного размещения фигуры на сетке игрового поля
 def valid_space(shape, grid):
     # список всех возможных позиций сетки экрана
-    # [[(0, 1)]], [[(2, 3)]] --> [[(0, 1), (2, 3)]]
+    # [[(0,1)], [[(2,3)]] -> [[(0,1), (2,3)]]
     accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
     accepted_pos = [j for sub in accepted_pos for j in sub]
     # отформатированные позиции
     formatted = convert_shape_format(shape)
 
-    # Цикл по всем отформатированным позициям
+    # Для всех отформатированных позиций
     for pos in formatted:
         # если позиция не содержится в списке возможных позиций
         if pos not in accepted_pos:
@@ -207,20 +215,20 @@ def check_lost(positions):
 
 # функция получения фигуры
 def get_shape():
-    # возвращаем случайную фигуру из списка возможных
+    # возвращаем случайную фигуру из списка возможных фигур
     return Piece(5, 0, random.choice(shapes))
 
 
 # функция отрисовки текста посередине
-def draw_text_middle(surface, text, size, color):
-    # устанавливаем шрифт для отображения текста посередине
-    font = pygame.font.SysFont('comicsans', size, bold=True)
-    # задаём текст для отображения
+def draw_text_middle(surface, text, size, color, ):
+    # устанавливаем шрифт для отображания
+    font = pygame.font.SysFont("comicsans", size, bold=True)
+    # отображаем шрифт на экране
     label = font.render(text, 1, color)
 
-    # отображаем текст в середине экрана
-    surface.blit(label, (TOP_LEFT_X + PLAY_WIDTH / 2 - (label.get_width() / 2),
-                         TOP_LEFT_Y + PLAY_HEIGHT / 2 - label.get_height() / 2))
+    # отображаем шрифт в середине экрана
+    surface.blit(label, (TOP_LEFT_X + PLAY_WIDTH / 2 - (label.get_width() / 2), TOP_LEFT_Y + PLAY_HEIGHT / 2
+                         - label.get_height() / 2))
 
 
 # функция отрисовки сетки игрового поля
@@ -231,15 +239,15 @@ def draw_grid(surface, grid):
     sy = TOP_LEFT_Y
     # На экране рисуем сетку в виде серых линий
     for i in range(len(grid)):
-        # Рисуем горизонтальные линии
+        # Горизонтальные линии
         pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * BLOCK_SIZE), (sx + PLAY_WIDTH, sy + i * BLOCK_SIZE))
         for j in range(len(grid[i])):
-            # Рисуем вертикальные линии
+            # Вертикальные линии
             pygame.draw.line(surface, (128, 128, 128), (sx + j * BLOCK_SIZE, sy),
                              (sx + j * BLOCK_SIZE, sy + PLAY_HEIGHT))
 
 
-# функция очистки игровой сетки
+# Функция "очистки" строк
 def clear_rows(grid, locked):
     # переменная счётчика (инкремент)
     inc = 0
@@ -252,31 +260,31 @@ def clear_rows(grid, locked):
             inc += 1
             # запоминаем индекс строки
             ind = i
-            # пытаемся удалить строку из словаря занятых позиций на игровой сетке
+            # пытаемся удалить строки из словаря заблокированных позиций на сетке
             for j in range(len(row)):
                 try:
                     del locked[(j, i)]
-                # в противном случае - продолжаем обход в цикле
+                # в противном случае - продолжаем обход цикла
                 except:
                     continue
-        # cдвигаем все оставшиеся строки на то кол-во строк, которое было удалено,
-        # путём добавления их в начало списка сетки игры
-        # [(0, 1), (0, 0)] --> [(0, 0), (0, 1)]
-        if inc > 0:
-            for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
-                x, y = key
-                if y < ind:
-                    newKey = (x, y + inc)
-                    locked[newKey] = locked.pop(key)
-        return inc
+    # сдвигаем всё оставшиеся строки на кол-во строк, которое было удалено,
+    # путём добавления их в начало списка сетки игры
+    # [(0, 1), (0, 0)] -> [(0, 0), (0, 1)]
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
+    return inc
 
 
-# функция отрисовки следующей игровой фигуры
+# Функция показа следующей фигуры
 def draw_next_shape(shape, surface):
     # установка шрифта для показа следующей фигуры
     font = pygame.font.SysFont('comicsans', 30)
     label = font.render('Следующая:', 1, (255, 255, 255))
-    # начальные координаты x и y для показа следующей фигуры
+    # Начальные координаты x и y для показа следующей фигуры
     sx = TOP_LEFT_X + PLAY_WIDTH + 50
     sy = TOP_LEFT_Y + PLAY_HEIGHT / 2 - 100
     format = shape.shape[shape.rotation % len(shape.shape)]
@@ -288,42 +296,41 @@ def draw_next_shape(shape, surface):
                 pygame.draw.rect(surface, shape.color, (sx + j * BLOCK_SIZE, sy + i * BLOCK_SIZE,
                                                         BLOCK_SIZE, BLOCK_SIZE), 0)
     # отображаем текст "следующая фигура" по заданным координатам x и y
-    surface.blit(label, (sx + 10, sy - 30))
+    surface.blit(label, (sx + 10, sy - 20))
 
 
-# функция подсчёта очков и записи их в файл
+# Подсчёт очков и их запись в файл
 def update_score(nscore):
     score = max_score()
 
-    with open('score.txt', 'w') as f:
+    with open('scores.txt', 'w') as f:
         if int(score) > nscore:
             f.write(str(score))
         else:
             f.write(str(nscore))
 
 
-# функция определения максимального количества очков
+# функция определения максимального кол-ва очков
 def max_score():
-    with open('score.txt', 'r') as f:
+    with open('scores.txt', 'r') as f:
         lines = f.readlines()
         score = lines[0].strip()
-
     return score
 
 
-# функция отрисовки окна игрового поля
-def draw_window(surface, grid, score=0, last_score=0):
+# функция отрисовки окна игры
+def draw_window(surface, grid, score=0, last_score=0, level=1):
     # заполняем экран игры чёрным цветом
     surface.fill((0, 0, 0))
 
     # инициализация шрифтов в pygame
     pygame.font.init()
-    # устанавливаем нужный шрифт
+    # устанавливаем шрифт
     font = pygame.font.SysFont('comicsans', 60)
     # отображаем название игры на экране белым цветом
     label = font.render('ТЕТРИС', 1, (255, 255, 255))
 
-    # показываем название игры в центре экрана
+    # отображаем название игры в центре экрана
     surface.blit(label, (TOP_LEFT_X + PLAY_WIDTH / 2 - (label.get_width() / 2), 30))
 
     # установка шрифта для показа текущих очков
@@ -333,10 +340,17 @@ def draw_window(surface, grid, score=0, last_score=0):
     sx = TOP_LEFT_X + PLAY_WIDTH + 50
     sy = TOP_LEFT_Y + PLAY_HEIGHT / 2 - 100
     surface.blit(label, (sx + 10, sy + 160))
+    # отображения игрового уровня
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Уровень: ' + str(level), 1, (255, 255, 255))
+    # Начальные координаты x и y для показа уровня игры
+    sx = TOP_LEFT_X - 200
+    sy = TOP_LEFT_Y + 200
+    surface.blit(label, (sx + 10, sy + 110))
     # отображение рекордов по очкам
     font = pygame.font.SysFont('comicsans', 30)
     label = font.render('Рекорд: ' + last_score, 1, (255, 255, 255))
-    # Начальные координаты x и y для показа рекордов по очкам
+    # Начальные координаты x и y для показа следующей фигуры
     sx = TOP_LEFT_X - 200
     sy = TOP_LEFT_Y + 200
     surface.blit(label, (sx + 10, sy + 160))
@@ -348,16 +362,16 @@ def draw_window(surface, grid, score=0, last_score=0):
     pygame.draw.rect(surface, (255, 0, 0), (TOP_LEFT_X, TOP_LEFT_Y, PLAY_WIDTH, PLAY_HEIGHT), 4)
 
     draw_grid(surface, grid)
-    pygame.display.update()
+    # pygame.display.update()
 
 
-# основная функция игры
+# основная функции игры
 def main(win):
-    # рекорд по очкам:
+    # последнее значение очков
     last_score = max_score()
-    # словарь "занятых" (заблокированных) позиций сетки игрового поля
+    # словарь заблокированных позиций сетки
     locked_positions = {}
-    # создание сетки из словар "занятых" позиций сетки игрового поля
+    # создание сетки из словаря заблокированных позиций
     grid = create_grid(locked_positions)
 
     # изменение фигуры
@@ -366,22 +380,22 @@ def main(win):
     run = True
     # текущая фигура
     current_piece = get_shape()
-    # cледующая фигура
+    # следующая фигура
     next_piece = get_shape()
     # внутриигровое время
     clock = pygame.time.Clock()
     # время падения фигуры
     fall_time = 0
     # скорость падения фигуры
-    fall_speed = 0.70
-    # время увеличения скорости падения фигур на конкретном уровне
+    fall_speed = 1.20
+    # время увеличения скорости падения фигур на уровне
     level_time = 0
-    # заработанные очки
+    # заработанные игроком очки
     score = 0
 
-    # оcновной игровой цикл
+    # основной игровой цикл
     while run:
-        # создаём сетку из "занятых" позиций
+        # создаём сетку из заблокированных позиций
         grid = create_grid(locked_positions)
         # высчитываем время падения, исходя из разницы времён
         # между предыдущей итерацией цикла и текущей
@@ -390,20 +404,20 @@ def main(win):
         level_time += clock.get_rawtime()
         clock.tick()
 
-        # если время падения фигуры на уровне больше 5 секунд
+        # если время падения фигур на уровне больше 5 секунд
         if level_time / 1000 > 5:
             level_time = 0
         # если скорость падения фигуры больше, чем 0.12
         if fall_speed > 0.12:
-            # то мы уменьшаем её на 0.007
+            # уменьшаем её на 0.007
             fall_speed -= 0.007
-        # если время падения фигуры / 1000 больше, чем скорость падения
+            # если время падения фигуры / 1000 больше, чем скорость падения
         if fall_time / 1000 > fall_speed:
-            #  то мы обнуляем время падения
+            # обнуляем время падения
             fall_time = 0
-            # уменьшаем координату y текущей фигуры на 1
+            # увеличиваем координату y фигуры на 1
             current_piece.y += 1
-            # если пространства сетки игрового поля не хватает, и координата y текущей фигуры больше 0
+            # если пространства сетки не хватает и координата y текущей фигуры больше 0
             if not (valid_space(current_piece, grid)) and current_piece.y > 0:
                 # уменьшаем координату y на 1
                 current_piece.y -= 1
@@ -415,25 +429,24 @@ def main(win):
             if event.type == pygame.QUIT:
                 run = False
                 pygame.display.quit()
-            # если зарегистрировано событие нажатия клавиш на клавиатуре
+            # если зарегистрировано событие нажатия клавиш
             if event.type == pygame.KEYDOWN:
                 # если нажата клавиша "стрелка влево"
                 if event.key == pygame.K_LEFT:
-                    # уменьшаем на 1  координату x текущей фигуры
+                    # уменьшаем на 1 текущую координату x фигуры
                     current_piece.x -= 1
-                    # если пространства достаточно
                     if not (valid_space(current_piece, grid)):
-                        # увеличиваем координату x текущей фигуры на 1
                         current_piece.x += 1
                 # если нажата клавиша "стрелка вправо"
                 if event.key == pygame.K_RIGHT:
-                    # увеличиваем на 1 координату x текущей фигуры
+                    # увеличиваем на 1 текущую координату x фигуры
                     current_piece.x += 1
+                    # если пространство не позволяет, то меняем фигуру
                     if not (valid_space(current_piece, grid)):
                         current_piece.x -= 1
                 # если нажата клавиша "стрелка вниз"
                 if event.key == pygame.K_DOWN:
-                    # увеличиваем на 1 координату y текущей фигуры
+                    # увеличиваем на 1 текущую координату y фигуры
                     current_piece.y += 1
                     if not (valid_space(current_piece, grid)):
                         current_piece.y -= 1
@@ -450,53 +463,52 @@ def main(win):
             x, y = shape_pos[i]
             if y > -1:
                 grid[y][x] = current_piece.color
-
         # если фигура сменилась
         if change_piece:
             for pos in shape_pos:
                 p = (pos[0], pos[1])
-                # {(1, 2): (255, 0, 0)}
+                # {(1,2): (255,0, 0)}
                 locked_positions[p] = current_piece.color
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
             # очищаем строки и добавляем их в кол-во заработанных игроком очков
             score += clear_rows(grid, locked_positions) * 10
-
         # вызов функции отрисовки основного окна игры
         draw_window(win, grid, score, last_score)
         # вызов функции отображения следующей фигуры
         draw_next_shape(next_piece, win)
         pygame.display.update()
-        # если всё игровое поле заполнено, то игрок - проиграл
+        # если все позиции игрового поля заняты фигурами
         if check_lost(locked_positions):
             draw_text_middle(win, "ВЫ ПРОИГРАЛИ!", 80, (255, 255, 255))
             pygame.display.update()
-            # задержка в 1.5 сек по времени
             pygame.time.delay(1500)
             run = False
+            # вызов функции обновления и записи очков
             update_score(score)
     # pygame.display.quit()
 
 
-# функция отображения главного меню
+# функция отображения главного меню игры
 def main_menu(win):
     # main(win)
     run = True
     while run:
         # отображаем инструкции игроку
         win.fill((0, 0, 0))
-        draw_text_middle(win, "НАЖМИТЕ ЛЮБУЮ КЛАВИШУ ДЛЯ НАЧАЛА", 30, (255, 255, 255))
+        draw_text_middle(win, 'НАЖМИТЕ ЛЮБУЮ КЛАВИШУ ДЛЯ НАЧАЛА', 30, (255, 255, 255))
         pygame.display.update()
         for event in pygame.event.get():
-            # если возникло событие завершения игры
+            # если возникло событие заверешения игры
             if event.type == pygame.QUIT:
                 # выходим из игрового цикла и прекращаем выполнение программы
                 run = False
-            # если возникло событие нажатия любой клавиши
+            # если возникло события нажатия любой клавиши
             if event.type == pygame.KEYDOWN:
                 # запускаем игру
                 main(win)
+
     pygame.display.quit()
 
 
